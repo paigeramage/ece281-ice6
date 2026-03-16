658e34915586e1b7dc86ae15dbbb476be2d69347
paigeramage <c28paige.ramage@afacademy.af.edu>
2026-03-15 20:04:48 -0600

    good waveform

--- src/TDM4.vhd
+++ src/TDM4.vhd
@@ -68,7 +68,7 @@ end TDM4;
 
 architecture behavioral of TDM4 is
 
-	signal   f_sel		 : unsigned(1 downto 0)	:= "00"; -- 2 bit counter output to select MUX input
+	signal   f_sel	: unsigned(1 downto 0)	:= "00"; -- 2 bit counter output to select MUX input
 	
 begin	
 	
